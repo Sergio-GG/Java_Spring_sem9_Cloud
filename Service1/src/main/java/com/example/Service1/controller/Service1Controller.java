55f31e836dca542375b39bf7c5037a5a3ea24d0a
Sergio <sergio-guitar@yandex.ru>
2024-05-24 19:05:03 +0300

    Cloud task

---
+++ Service1/src/main/java/com/example/Service1/controller/Service1Controller.java
@@ -0,0 +1,16 @@
+package com.example.Service1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service1")
+public class Service1Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Service1";
+    }
+
+}
